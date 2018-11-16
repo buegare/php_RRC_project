3f680b454ac976e07a1f0bec40d9491aa2df05b9
buegare <brunobuegare@gmail.com>
2018-11-16 02:04:52 -0600

    check if constants are already defined

--- connect.php
+++ connect.php
@@ -1,7 +1,15 @@
 <?php
-    define('DB_DSN','mysql:host=localhost;dbname=gagroup;charset=utf8');
-    define('DB_USER','admin');
-    define('DB_PASS','admin');
+    if (!defined('DB_DSN')) {
+      define('DB_DSN','mysql:host=localhost;dbname=gagroup;charset=utf8');
+    }
+
+    if (!defined('DB_USER')) {
+      define('DB_USER','admin');
+    }
+
+    if (!defined('DB_PASS')) {
+      define('DB_PASS','admin');
+    }    
 
     try {
       $db = new PDO(DB_DSN, DB_USER, DB_PASS);
